e771e84277820aef33fc5e189b1355f01ee144ef
Tenaw <mete439@gmail.com>
2020-01-23 19:38:40 -0800

    url changed

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ function eventHandler(request, response) {
   let eventName = request.query.city_name;
   let eventDay = request.query.event_date;
 
-  const url = `http://api.eventful.com/json/events/search?&keywords=${process.env.EVENTFUL_API_KEY}&location=${eventName}&${eventDay}=Future`;
+  const url = `http://api.eventful.com/json/events/search?&keywords=books/${process.env.EVENTFUL_API_KEY}&location=${eventName}&${eventDay}=Future`;
 
 
 
